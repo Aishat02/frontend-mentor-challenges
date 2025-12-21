3c1d80a08ee73362adc526237eeb8b8eabea7ebd
Aris <titilayotijani15@gmail.com>
2025-12-21 19:56:39 +0100

    Squashed 'article-preview-component-main/' content from commit 8d86357
    git-subtree-dir: article-preview-component-main
    git-subtree-split: 8d86357bfca648237338a845dfccb2479f128172

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let shareButton = document.querySelector(".share-container");
+function contactLinks() {
+  shareButton.classList.toggle("active");
+  document.querySelector("main").classList.toggle("active");
+}
